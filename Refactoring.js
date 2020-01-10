577dda35063fed057ed783bf5c6f0cad68a0fed2
Jessica Pope <jessica.pope.co@gmail.com>
2020-01-10 11:49:54 -0700

    Add solutions

--- Refactoring.js
+++ Refactoring.js
@@ -1,22 +1,30 @@
 // Refactor the Code Below Using the ES6 Method Listed
 // Additionally, Prepare an Explanation for Why We Would
-// Would Want to Refactor Using that Method
+// Would or Wouldn't Want to Refactor Using that Method
 
 // Spread
 //combine the follow objects into an new object using the spread operator
 const dog1 = {firstName: "Buckley", breed: "Lab Mix"}
 const dog2 = {firstName: "Ginny", breed: "American Fox Hound"}
 
-
+//solution
+const dogs = {...dog1, ...dog2}
 
 // Destructuring
 //desctrure the following object into the object below
 const buckley = {fullName: "Wiggley F. Buckley", favoriteFood: "Peanutbutter", favoriteActivity:"Dog Parking", leastFavoriteActivity:"riding in the car", bFF:"Diesel", }
 
+//solution
+const {fullName, bFF} = dogPairs
 
 //result
 dogPairs => {"Wiggley F. Buckley", "Diesel"}
 
 
-
 // Arrow
+buttonElement.addEventListener('click', function(event){
+  alert('element clicked!')
+})
+
+//solution
+buttonElement.addEventListener('click', ()=> alert('element clicked!'))
