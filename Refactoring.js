e549a5d3f7e5bd4032c6653a2608b8e9fc17a475
Jessica Pope <jessica.pope.co@gmail.com>
2020-01-10 11:09:06 -0700

    Comment instructions

---
+++ Refactoring.js
@@ -0,0 +1,14 @@
+// Refactor the Code Below Using the ES6 Method Listed
+// Additionally, Prepare an Explanation for Why We Would
+// Would Want to Refactor Using that Method
+
+// Spread
+
+
+
+
+// Destructuring
+
+
+
+// Arrow
