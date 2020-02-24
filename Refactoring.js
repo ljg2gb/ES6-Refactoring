5182a37eed9767a0c45f236289c7e25ebad80c7c
Jessica Pope <jessica.pope.co@gmail.com>
2020-02-24 13:58:30 -0700

    clears solutions

--- Refactoring.js
+++ Refactoring.js
@@ -10,9 +10,9 @@ const dog1 = {firstName: "Buckley", breed: "Lab Mix"}
 const dog2 = {firstName: "Ginny", breed: "American Fox Hound"}
 
 
-const newObject = {...dog1, ...dog2}
+
 // result
-console.log(newObject)
+console.log(newObject) = {firstName: "Ginny", breed: "American Fox Hound"}
 
 
 
@@ -21,10 +21,10 @@ console.log(newObject)
 //consider first how you would do it without destructuring
 const buckley = {fullName: "Wiggley F. Buckley", favoriteFood: "Peanutbutter", favoriteActivity:"Dog Parking", leastFavoriteActivity:"riding in the car", bFF:"Diesel", }
 
-const {fullName, bFF} = buckley
+
 
 //result
-console.log(fullName)
+console.log(fullName) = "Wiggley F. Buckley"
 
 
 
@@ -32,10 +32,10 @@ console.log(fullName)
 //use Object.assign() to non-destructively combine the cats object and your newObjectup above into a "pets" object
 const cats = {fullName:'Pepper', breed:'Persian'}
 
-const pets = Object.assign({},newObject, cats)
+
 
 
 //result
-console.log(pets)
+console.log(pets) = {firstName: "Ginny", breed: "American Fox Hound", fullName:'Pepper', breed:'Persian'}
 
 })
