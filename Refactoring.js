23589af6c93ae5aafa74c9fb8a553e6387ad1a3f
Jessica Pope <jessica.pope.co@gmail.com>
2020-02-24 13:27:04 -0700

    add in new

--- Refactoring.js
+++ Refactoring.js
@@ -1,3 +1,4 @@
+document.addEventListener('DOMContentLoaded', ()=>{
 // Refactor the Code Below Using the ES6 Method Listed
 // Additionally, Prepare an Explanation for Why We Would or Wouldn't
 // Would Want to Refactor Using that Method
@@ -10,16 +11,12 @@ const dog2 = {firstName: "Ginny", breed: "American Fox Hound"}
 
 
 // Destructuring
-//desctrure the following object into the object below
+//desctrure the following object to produce the result below
+//consider first how you would do it without destructuring
 const buckley = {fullName: "Wiggley F. Buckley", favoriteFood: "Peanutbutter", favoriteActivity:"Dog Parking", leastFavoriteActivity:"riding in the car", bFF:"Diesel", }
 
 
 //result
-dogPairs => {"Wiggley F. Buckley", "Diesel"}
+console.log(fullName) = "Wiggley F. Buckley"
 
-
-
-// Arrow
-buttonElement.addEventListener('click', function(event){
-  alert('element clicked!')
 })
